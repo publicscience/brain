86614ffcdb6caa338eb3ff9321b8a358f9cd3557
Francis Tseng <ftzeng@gmail.com>
2013-12-15 17:53:43 -0500

    some reorganization

--- app/__init__.py
+++ app/__init__.py
@@ -9,26 +9,4 @@ app.config.from_object('config')
 # Setup the database.
 db = MongoEngine(app)
 
-from app.models import Config
-
-# Create Config object if necessary.
-if len(Config.objects) < 1:
-    config = Config()
-    config.save()
-
-def config():
-    """
-    Loads the Config.
-    Loading it on the fly means the latest
-    configuration will always be used.
-    """
-    return Config.objects[0]
-
-from app import routes, brain
-import schedule
-
-"""
-Fetch tweets and memorize and process them
-every hour.
-"""
-#schedule.every().hour.do(brain.ponder)
+from app import routes, jobs, config
---
+++ app/jobs.py
@@ -0,0 +1,8 @@
+from app import brain
+import schedule
+
+"""
+Fetch tweets and memorize and process them
+every hour.
+"""
+#schedule.every().hour.do(brain.ponder)
