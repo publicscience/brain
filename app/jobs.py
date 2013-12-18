d1f042115a89cf8e803d91599b6e926e118fad65
Francis Tseng <ftzeng@gmail.com>
2013-12-17 23:58:46 -0500

    added and reactivated scheduled jobs

--- app/jobs.py
+++ app/jobs.py
@@ -5,4 +5,9 @@ import schedule
 Fetch tweets and memorize and process them
 every hour.
 """
-#schedule.every().hour.do(brain.ponder)
+schedule.every().hour.do(brain.ponder)
+
+"""
+Every 10 minutes consider tweeting something poignant.
+"""
+schedule.every(10).minutes.do(brain.consider)
