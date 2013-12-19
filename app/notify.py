f98d6b18b6ac77d256ba81aadc5600c7975ea097
Francis Tseng <ftzeng@gmail.com>
2013-12-19 00:17:46 -0500

    mail config fix

--- app/notify.py
+++ app/notify.py
@@ -1,10 +1,12 @@
 from app import app
 
+cfg = app.config
+
 # Email error messages.
 if not app.debug:
     import logging
     from logging.handlers import SMTPHandler
-    mail_handler = SMTPHandler((MAIL_HOST, MAIL_PORT), 'dont-talk-back@'+MAIL_HOST, MAIL_TARGETS, 'brain is floudering!', (MAIL_USER, MAIL_PASS))
+    mail_handler = SMTPHandler((cfg['MAIL_HOST'], cfg['MAIL_PORT']), 'dont-talk-back@'+cfg['MAIL_HOST'], cfg['MAIL_TARGETS'], 'brain is floudering!', (cfg['MAIL_USER'], cfg['MAIL_PASS']))
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
 
