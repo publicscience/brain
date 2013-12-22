6c49facb20ac7d8fb24366b3d278871874f417f8
Francis Tseng <ftzeng@gmail.com>
2013-12-22 01:32:31 -0500

    scheduling fix

--- app/jobs.py
+++ app/jobs.py
@@ -26,7 +26,7 @@ class ScheduleThread(threading.Thread):
     @classmethod
     def run(cls):
         while not cease_continuous_run.is_set():
-            schedule.run_pending()
+            schedule.default_scheduler.run_pending()
             time.sleep(1)
 
 continuous_thread = ScheduleThread()
